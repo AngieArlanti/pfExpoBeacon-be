d99944ea27e3dcbff7be986dcd2dc914b5da909c
AngieArlanti <angeles.arlanti@mercadolibre.com>
2020-02-01 21:04:58 -0300

    Refactor StandVisitHoursRepository

--- src/main/java/api/stats/application/StatsService.java
+++ src/main/java/api/stats/application/StatsService.java
@@ -1,12 +1,13 @@
 package api.stats.application;
 
-import api.stats.domain.*;
+import api.stats.domain.ExpoHours;
+import api.stats.domain.ExpoHoursRepository;
+import api.stats.domain.StandVisitHours;
+import api.stats.domain.StandVisitHoursRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class StatsService {
@@ -18,22 +19,15 @@ public class StatsService {
 
     private StandVisitHoursMapper standVisitHoursMapper = new StandVisitHoursMapper();
 
-    /**
-     * TODO jdoc
-     * @param standId
-     * @return
+    /** Returns amount of visits per hour for the given Stand Id.
+     *
+     * @param standId to calculate amount of visits per hour.
+     * @return amount of visits per hour for the given Stand Id.
      */
     List<StandVisitHoursDto> getStandVisitHours(final String standId){
+        List<StandVisitHours> standVisitHours = standVisitHoursRepository.findByStandId(standId);
         List<ExpoHours> expoHours = expoHoursRepository.findAll();
-        StandVisitHoursId id;
-        List<StandVisitHoursDto> standVisitHoursDtos = new ArrayList<>();
-        for (ExpoHours expoHour : expoHours) {
-            id = new StandVisitHoursId(standId, expoHour.getId());
-            //TODO refactor findByStandId!!!
-            Optional<StandVisitHours> standVisitHours = standVisitHoursRepository.findById(id);
-            standVisitHours.ifPresent(standVisitHours1 ->
-                    standVisitHoursDtos.add(standVisitHoursMapper.toDto(standVisitHours1, expoHour)) );
-        }
-        return standVisitHoursDtos;
+
+        return standVisitHoursMapper.toDto(standVisitHours, expoHours);
     }
 }
--- src/main/java/api/stats/domain/StandVisitHoursRepository.java
+++ src/main/java/api/stats/domain/StandVisitHoursRepository.java
@@ -1,7 +1,11 @@
 package api.stats.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StandVisitHoursRepository extends JpaRepository<StandVisitHours, StandVisitHoursId> {
-
+    List<StandVisitHours> findByStandId(final String standId);
 }
