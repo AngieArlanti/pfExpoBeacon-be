9c4fd16b98ba8a2aacf6c0ddbf9a16afee3baa2d
Agop Hurmuz <agophurmuz@gmail.com>
2020-03-10 22:27:19 -0300

    configs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,11 +6,12 @@ INSERT INTO ranking_average(ranking, cant_rates) VALUES (3,1);
 INSERT INTO stand(id, title, short_description, description, cover, ranking_average_id, latitude, longitude, average_time) VALUES('0C:F3:EE:08:FC:DD', 'Combot', 'Un proyecto con eje ecológico',
 'Un proyecto con eje ecológico. Conocé ComBot,  la compactadora de botellas PET creada en su totalidad por Martín Pérez Mendoza y Santiago Carrillo.', 'https://user-images.githubusercontent.com/7771294/68998284-32920f80-088f-11ea-86aa-648135b7d262.jpg', 1,-34.641176, -58.401177, 10);
 
-INSERT INTO stand(id, title, short_description, description, cover, ranking_average_id, latitude, longitude, average_time) VALUES('0C:F3:EE:04:19:2F', 'Auto Formula SAE',
-'Creado por estudiantes de Ingeniería Mecánica del ITBA','Auto Fórmula SAE creado por estudiantes de Ingeniería Mecánica del ITBA.', 'https://user-images.githubusercontent.com/7771294/68998234-83553880-088e-11ea-8763-b01236188de6.jpg', 2,-34.641262, -58.401164, 10);
-
 INSERT INTO stand(id, title, short_description, description, cover, ranking_average_id, latitude, longitude, average_time) VALUES('0C:F3:EE:04:18:A0', 'SYNTHETIC VOICE HARMONIZATION',
-'Software para armonizar sonidos musicales', 'El proyecto se basa en el diseño de un software para armonizar sonidos musicales. Un armonizador es una aplicación de hardware o software que toma una señal de audio y le suma su tercera voz (los sonidos de la señal desplazados en su escala musical). El programa recibe un archivo de audio, calcula la escala de la pieza musical, determina las notas y las desplaza en su escala para obtener la tercera voz. Esta voz es sumada a la original, creando un efecto de armonización. Se utilizan algoritmos de procesamiento de audio, como el McLeud Pitch Method y un phase vocoder, con ciertas modificaciones ideadas para obtener la implementación deseada.', 'https://user-images.githubusercontent.com/7771294/68998580-1b552100-0893-11ea-8384-42a22b6d901a.jpeg', 3,-34.641254, -58.401091, 10);
+'Software para armonizar sonidos musicales', 'El proyecto se basa en el diseño de un software para armonizar sonidos musicales. Un armonizador es una aplicación de hardware o software que toma una señal de audio y le suma su tercera voz (los sonidos de la señal desplazados en su escala musical). El programa recibe un archivo de audio, calcula la escala de la pieza musical, determina las notas y las desplaza en su escala para obtener la tercera voz. Esta voz es sumada a la original, creando un efecto de armonización. Se utilizan algoritmos de procesamiento de audio, como el McLeud Pitch Method y un phase vocoder, con ciertas modificaciones ideadas para obtener la implementación deseada.', 'https://user-images.githubusercontent.com/7771294/68998580-1b552100-0893-11ea-8384-42a22b6d901a.jpeg', 2,-34.641262, -58.401164, 10);
+
+INSERT INTO stand(id, title, short_description, description, cover, ranking_average_id, latitude, longitude, average_time) VALUES('0C:F3:EE:04:19:2F', 'Auto Formula SAE',
+'Creado por estudiantes de Ingeniería Mecánica del ITBA','Auto Fórmula SAE creado por estudiantes de Ingeniería Mecánica del ITBA.', 'https://user-images.githubusercontent.com/7771294/68998234-83553880-088e-11ea-8763-b01236188de6.jpg', 3,-34.641254, -58.401091, 10);
+
 
 INSERT INTO stand(id, title, short_description, description, cover, ranking_average_id, latitude, longitude, average_time) VALUES('0C:F3:EE:04:19:8E', 'GAZE TRACKER',
 'Detección y seguimiento de pupilas', 'Es un sistema de detección y seguimiento de pupilas utilizando una webcam, aplicado a la estimación de la posición de la vista del usuario.', 'https://user-images.githubusercontent.com/7771294/68998629-c239bd00-0893-11ea-9f84-9cf179282c6d.jpeg', 4,-34.641163, -58.401110, 10);
@@ -59,18 +60,18 @@ INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 2, 10);
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 3, 10);
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 4, 10);
-INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 5, 0);
-INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 6, 0);
-INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 7, 0);
+INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 5, 5);
+INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 6, 5);
+INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:2F', 7, 5);
 
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 0, 0);
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 1, 0);
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 2, 0);
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 3, 0);
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 4, 0);
-INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 5, 5);
-INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 6, 5);
-INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 7, 5);
+INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 5, 0);
+INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 6, 0);
+INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:18:A0', 7, 0);
 
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:8E', 0, 2);
 INSERT INTO stand_visit_hours(stand_id, expo_hours_id, visits) VALUES('0C:F3:EE:04:19:8E', 1, 2);
